76c714f2e4127b168b7ef2f3eff5f35179a04d45
Unknown <claudiu@megheaiulian.com>
2019-11-24 18:56:21 +0200

    Remove temporary navigation and scroll button

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -7,7 +7,7 @@ export default class Header extends Component {
     return (
       <React.Fragment>
         <header id="home">
-          <HeaderNav />
+          {/* <HeaderNav /> */}
           <div class="row banner">
             <div class="banner-text">
               <h1 class="responsive-headline">
@@ -18,7 +18,7 @@ export default class Header extends Component {
             </div>
           </div>
           <p class="scrolldown">
-            <a class="smoothscroll" href="#about"><i class="icon-down-circle"></i></a>
+            {/* <a class="smoothscroll" href="#about"><i class="icon-down-circle"></i></a> */}
           </p>
         </header>
       </React.Fragment>
