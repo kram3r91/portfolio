078886efbf8b2830b0eacecf6b5c40749e53fca4
Unknown <claudiu@megheaiulian.com>
2019-11-24 18:55:46 +0200

    Update socials

--- src/components/header/socials.js
+++ src/components/header/socials.js
@@ -4,13 +4,10 @@ export default class Header extends Component {
     return (
       <React.Fragment>
         <ul class="social">
-          <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-          <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-          <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-          <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-          <li><a href="#"><i class="fa fa-instagram"></i></a></li>
-          <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
-          <li><a href="#"><i class="fa fa-skype"></i></a></li>
+          <li><a href="https://www.facebook.com/kram3rRO" target="_blank"><i class="fa fa-facebook"></i></a></li>
+          <li><a href="https://www.linkedin.com/in/claudiu-ciprian/" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+          <li><a href="https://www.instagram.com/claudiu.cip/" target="_blank"><i class="fa fa-instagram"></i></a></li>
+          <li><a href="skype:live:todirasclaudiu_1?chat"><i class="fa fa-skype"></i></a></li>
         </ul>
       </React.Fragment>
     );
