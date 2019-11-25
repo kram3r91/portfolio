8e53cccc78aa4675fec0acce838705adb457fdb3
Unknown <claudiu@megheaiulian.com>
2019-11-25 19:52:18 +0200

    Navigation component changes

--- src/components/header/nav.js
+++ src/components/header/nav.js
@@ -1,20 +1,29 @@
 import React, { Component } from 'react';
+import '../../css/navigation.css';
+
 export default class Header extends Component {
   render() {
     return (
       <React.Fragment>
-          <nav id="nav-wrap">
-            <a class="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
-            <a class="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
-            <ul id="nav" class="nav">
-              <li class="current"><a class="smoothscroll" href="#home">Home</a></li>
-              <li><a class="smoothscroll" href="#about">About</a></li>
-              <li><a class="smoothscroll" href="#resume">Resume</a></li>
-              <li><a class="smoothscroll" href="#portfolio">Works</a></li>
-              <li><a class="smoothscroll" href="#testimonials">Testimonials</a></li>
-              <li><a class="smoothscroll" href="#contact">Contact</a></li>
+        <nav class="navbar navbar-expand-lg navbar-light">
+          <img class="navbar-brand" src="logo.png" width="50"/>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item active">
+                <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="about.html">about</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="contact.html">contact</a>
+              </li>
             </ul>
-          </nav>
+          </div>
+        </nav>
       </React.Fragment>
     );
   }
