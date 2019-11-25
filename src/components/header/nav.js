88c6aa8a4b3cf745546e028df199ef8b8b0cc9b4
Unknown <claudiu@megheaiulian.com>
2019-11-25 20:34:51 +0200

    Navbar changes

--- src/components/header/nav.js
+++ src/components/header/nav.js
@@ -6,7 +6,7 @@ export default class Header extends Component {
     return (
       <React.Fragment>
         <nav class="navbar navbar-expand-lg navbar-light">
-          <img class="navbar-brand" src="logo.png" width="50"/>
+          
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
