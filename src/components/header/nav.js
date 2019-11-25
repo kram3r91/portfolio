8b444fe98eace2f952a7e1049dd525abc9d4783b
Unknown <claudiu@megheaiulian.com>
2019-11-25 21:35:44 +0200

    Stage files

--- src/components/header/nav.js
+++ src/components/header/nav.js
@@ -6,17 +6,20 @@ export default class Header extends Component {
     return (
       <React.Fragment>
         <nav class="navbar navbar-expand-lg navbar-light">
-          
+          <img class="navbar-brand" src="logo.png" width="80"/>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
-              <li class="nav-item active">
+              <li class="nav-item">
                 <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="about.html">about</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="portfolio.html">portfolio</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="contact.html">contact</a>
