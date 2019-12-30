cc8069d2dc417723f56581f284c0be16c2ee5bdb
Unknown <claudiu@megheaiulian.com>
2019-12-30 19:22:44 +0200

    Add upwork to socials icons, css and rename class to className

--- src/components/home/socials.js
+++ src/components/home/socials.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFacebook, faLinkedin, faInstagram, faSkype } from '@fortawesome/free-brands-svg-icons'
+import { faLinkedin, faSkype } from '@fortawesome/free-brands-svg-icons';
+
+import upworkLogo from './upwork.svg';
 
 export default class Header extends Component {
   render() {
     return (
       <React.Fragment>
-        <div class="socials">
-          <a href="https://www.facebook.com/kram3rRO" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faFacebook} size="2x" /></a>
+        <div className="socials">
+          <a href="https://www.upwork.com/o/profiles/users/_~0183a72fd6c73daa4c/" target="_blank" rel="noopener noreferrer"><img src={upworkLogo} alt="upwork"/></a>
           <a href="https://www.linkedin.com/in/claudiu-ciprian/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} size="2x" /></a>
-          <a href="https://www.instagram.com/claudiu.cip/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faInstagram} size="2x" /></a>
           <a href="skype:live:todirasclaudiu_1?chat"><FontAwesomeIcon icon={faSkype} size="2x" /></a>
         </div>
       </React.Fragment>
