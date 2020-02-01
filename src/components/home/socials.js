e69dfb197a5315ca9d44b470e9f3a803ac3b323a
Unknown <claudiu@megheaiulian.com>
2020-02-01 21:11:20 +0200

    correct skype id

--- src/components/home/socials.js
+++ src/components/home/socials.js
@@ -12,7 +12,7 @@ export default class Header extends Component {
         <div className="socials">
           <a href="https://www.upwork.com/o/profiles/users/_~0183a72fd6c73daa4c/" target="_blank" rel="noopener noreferrer"><img src={upworkLogo} alt="upwork"/></a>
           <a href="https://www.linkedin.com/in/claudiu-ciprian/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} size="2x" /></a>
-          <a href="skype:live:todirasclaudiu_1?chat"><FontAwesomeIcon icon={faSkype} size="2x" /></a>
+          <a href="skype:live:todirasclaudiu?chat"><FontAwesomeIcon icon={faSkype} size="2x" /></a>
         </div>
       </React.Fragment>
     );
